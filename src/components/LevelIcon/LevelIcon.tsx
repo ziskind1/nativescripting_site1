a508665d38eeab2be1d60b3b1563be0c0f5b41dc
Alexander Ziskind <alex@nuvious.com>
2018-02-14 09:55:54 -0500

    Levelcon componentn added

---
+++ src/components/LevelIcon/LevelIcon.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { CourseLevel } from "../../domain/models";
+
+
+export interface LevelIconProps {
+    level: CourseLevel;
+}
+
+
+function getLevelIconImgSrc(level: CourseLevel): string {
+    switch (level.levelId) {
+        case 1:
+            return '/img/level_icon1.svg';
+        case 2:
+            return '/img/level_icon2.svg';
+        case 3:
+            return '/img/level_icon3.svg';
+    }
+}
+
+const LevelIcon: React.StatelessComponent<LevelIconProps> = (props: LevelIconProps) => {
+
+    const imgSrc = getLevelIconImgSrc(props.level);
+
+    return (
+        <img className="top-left-image" src={imgSrc} title={props.level.levelName} />
+    );
+};
+
+export default LevelIcon;
