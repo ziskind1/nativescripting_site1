eb36f98393cf36948688161e638ba36d7396f1c0
Alexander Ziskind <alex@nuvious.com>
2018-02-22 14:28:15 -0500

    added public folder

---
+++ public/path----662af614c21f8399b8d0.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{131:function(t,a){t.exports={data:{site:{siteMetadata:{siteName:"NativeScripting"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----662af614c21f8399b8d0.js.map
