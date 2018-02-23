498144f41f6c2b35e3e255b09ef72f62092d6ddf
Alexander Ziskind <alex@nuvious.com>
2018-02-23 08:20:00 -0500

    deleted public folder

--- public/path----662af614c21f8399b8d0.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{131:function(t,a){t.exports={data:{site:{siteMetadata:{siteName:"NativeScripting"}}},layoutContext:{}}}});
-//# sourceMappingURL=path----662af614c21f8399b8d0.js.map
