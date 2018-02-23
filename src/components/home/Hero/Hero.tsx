87bad67a39cbe061dddb312434c895723dbfef5c
Alexander Ziskind <alex@nuvious.com>
2018-02-22 22:48:07 -0500

    Changed Hero title casing

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -14,7 +14,7 @@ function Hero() {
         <div className="hero">
             <div className="hero-content">
                 <div className="wrapper">
-                    <h2 className="hero-main-title">Turn up your NativeScript training</h2>
+                    <h2 className="hero-main-title">Turn up your NativeScript Training</h2>
                     <p>
                         Upgrade your skills with online courses so you can master the NativeScript framework and build real world native mobile apps using your existing web development skills.
                     </p>
