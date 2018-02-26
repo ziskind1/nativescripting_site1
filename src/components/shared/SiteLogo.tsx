47b9a6db9ab4ea54d420c6cbac90696519307d20
Alexander Ziskind <alex@nuvious.com>
2018-02-26 17:25:31 -0500

    Added main header NativeScripting Icon instead of full logo for smaller screens

--- src/components/shared/SiteLogo.tsx
+++ src/components/shared/SiteLogo.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { withPrefix } from 'gatsby-link';
 
 const logoSrc = withPrefix('/img/logo_full.svg');
+const logoIconSrc = withPrefix('/img/logo_full_icon.svg');
 
 interface SiteLogoProps {
     siteName: string;
@@ -11,7 +12,8 @@ interface SiteLogoProps {
 function SiteLogo(props: SiteLogoProps) {
     return (
         <a href="/">
-            <img src={logoSrc} alt={props.siteName} />
+            <img className="site-logo-img" src={logoSrc} alt={props.siteName} />
+            <img className="site-logo-icon-img" src={logoIconSrc} alt={props.siteName} />
         </a>
     );
 }
