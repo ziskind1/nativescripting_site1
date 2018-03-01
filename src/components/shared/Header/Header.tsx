3a4f482ddab7987dc3159874140e9c6c6ee9e9f5
Alexander Ziskind <alex@nuvious.com>
2018-03-01 09:27:57 -0500

    Added posts to nav

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
+import Link from 'gatsby-link';
 
 import SiteLogo from '../SiteLogo';
 import Burger from "../Burger";
 import { MainMenuMobile, MainMenu } from "../MainMenu";
 
 import './Header.css';
+
 
 interface HeaderProps {
     siteName: string;
@@ -20,6 +22,7 @@ function Header(props: HeaderProps) {
             </div>
 
             <div className="header-nav-container">
+                <Link to='/posts'>Posts</Link>
                 <a href={'https://sso.teachable.com/secure/89912/users/sign_in?reset_purchase_session=1'} className="sign">Login</a>
             </div>
 
--- src/components/shared/MainMenu.tsx
+++ src/components/shared/MainMenu.tsx
@@ -5,7 +5,7 @@ export function getMenu(className: string) {
     return (
         <div className={className}>
             {/*<Link to={'/training'}>Training</Link>*/}
-            {/*<Link to='/posts'>Posts</Link>*/}
+            <Link to='/posts'>Posts</Link>
             <Link to={'/about'}>About</Link>
             <a href={'https://sso.teachable.com/secure/89912/users/sign_in?reset_purchase_session=1'} className="sign">Login</a>
         </div>
