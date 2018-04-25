516fa24a576579b3a7c24bc6cd0a1582ba614a34
Alexander Ziskind <alex@nuvious.com>
2018-04-25 08:59:51 -0400

    Added privacy and terms

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,25 +1,24 @@
 import * as React from "react";
 import { getMenu } from "../MainMenu";
 
-import './Footer.css';
+import "./Footer.css";
 
 function Footer() {
+  const year = new Date().getFullYear();
+  const menu = getMenu("");
 
-    const year = new Date().getFullYear();
-    const menu = getMenu('');
-
-    return (
-        <div className="wrapper">
-            <div className="footer-container">
-                <div>
-                    <p>@{year} NativeScripting</p>
-                </div>
-                <div>
-                    {menu}
-                </div>
-            </div>
+  return (
+    <div className="wrapper">
+      <div className="footer-container">
+        <div>@{year} NativeScripting</div>
+        <div className="privacy-links">
+          <a href="/terms">Terms of Use</a>
+          <a href="/privacy">Privacy Policy</a>
         </div>
-    );
+        <div>{menu}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
