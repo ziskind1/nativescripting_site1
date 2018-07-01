317efd5f80d90fe3d72af558ec8c62410ca09289
Alexander Ziskind <alex@nuvious.com>
2018-07-01 10:02:20 -0400

    added support for multiple authors on course detail page

---
+++ src/components/courseDetails/CourseAuthorsCard/CourseAuthorsCard.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Author } from "../../../domain/models";
+
+import "./CourseAuthorsCard.css";
+
+interface CourseAuthorsCardProps {
+  authors: Author[];
+}
+
+export const CourseAuthorsCard: React.StatelessComponent<
+  CourseAuthorsCardProps
+> = (props: CourseAuthorsCardProps) => {
+  const authors = props.authors;
+
+  const authorCardTitle = authors.length > 1 ? "Authors" : "Author";
+
+  const authorsHtml = authors.map(a => {
+    const authorImgSrc = `/img/authors/${a.picture}`;
+    return (
+      <div className="course-author-block">
+        <div className="course-author-name-block">
+          <div>
+            <h3>{a.name}</h3>
+            <h4>{a.title}</h4>
+          </div>
+          <div>
+            <img src={authorImgSrc} alt={a.name} title={a.name} />
+          </div>
+        </div>
+
+        <p className="card-bio">{a.bio}</p>
+      </div>
+    );
+  });
+
+  return (
+    <div className="course-author-card-container">
+      <p className="card-label">{authorCardTitle}</p>
+      {authorsHtml}
+    </div>
+  );
+};
--- src/components/courseDetails/CourseMetaCard/CourseMetaCard.tsx
+++ src/components/courseDetails/CourseMetaCard/CourseMetaCard.tsx
@@ -1,30 +1,25 @@
-import * as React from 'react';
-import { Course } from '../../../domain/models';
-import LevelIcon from '../../LevelIcon/LevelIcon';
-import { CourseAuthorCard } from '../CourseAuthorCard/CourseAuthorCard';
-import { CoursePurchaseArea } from '../CoursePurchaseArea/CoursePurchaseArea';
-import { CourseCoverage } from '../CourseCoverage/CourseCoverage';
+import * as React from "react";
+import { Course } from "../../../domain/models";
+import { CourseAuthorsCard } from "../CourseAuthorsCard/CourseAuthorsCard";
+import { CourseCoverage } from "../CourseCoverage/CourseCoverage";
 
-import './CourseMetaCard.css';
-
-
-
+import "./CourseMetaCard.css";
 
 interface CourseMetaCardProps {
-    course: Course;
+  course: Course;
 }
 
-export const CourseMetaCard: React.StatelessComponent<CourseMetaCardProps> = (props: CourseMetaCardProps) => {
+export const CourseMetaCard: React.StatelessComponent<CourseMetaCardProps> = (
+  props: CourseMetaCardProps
+) => {
+  const course = props.course;
 
-    const course = props.course;
+  return (
+    <div className="course-meta-card-container">
+      <h2>About this course</h2>
 
-
-    return (
-        <div className="course-meta-card-container">
-            <h2>About this course</h2>
-
-            <CourseAuthorCard author={course.authors[0]} />
-            <CourseCoverage course={course} />
-        </div>
-    );
+      <CourseAuthorsCard authors={course.authors} />
+      <CourseCoverage course={course} />
+    </div>
+  );
 };
