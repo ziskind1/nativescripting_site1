8466dbdc09c2f39e8efc7b3ca4aa279efaad3fb8
Alexander Ziskind <alex@nuvious.com>
2018-08-01 09:23:04 -0400

    Added new header link and updated footer link

--- src/components/shared/MainMenu.tsx
+++ src/components/shared/MainMenu.tsx
@@ -2,20 +2,27 @@ import * as React from 'react';
 import Link from 'gatsby-link';
 
 export function getMenu(className: string) {
-    return (
-        <div className={className}>
-            {/*<Link to={'/training'}>Training</Link>*/}
-            <Link to='/posts'>Posts</Link>
-            <Link to={'/about'}>About</Link>
-            <a href={'https://sso.teachable.com/secure/89912/users/sign_in?reset_purchase_session=1'} className="sign">Login</a>
-        </div>
-    );
+  return (
+    <div className={className}>
+      {/*<Link to={'/training'}>Training</Link>*/}
+      <Link to="/posts">Posts</Link>
+      <Link to={'/about'}>Authors</Link>
+      <a
+        href={
+          'https://sso.teachable.com/secure/89912/users/sign_in?reset_purchase_session=1'
+        }
+        className="sign"
+      >
+        Login
+      </a>
+    </div>
+  );
 }
 
 export function MainMenuMobile() {
-    return getMenu('mobile-header');
+  return getMenu('mobile-header');
 }
 
 export function MainMenu() {
-    return getMenu('header-page');
+  return getMenu('header-page');
 }
