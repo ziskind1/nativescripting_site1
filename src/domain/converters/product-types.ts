9c64615e0fc009983ff4dc141043a7736fea7cf1
Alexander Ziskind <alex@nuvious.com>
2018-11-21 22:07:17 -0500

    user number rename

--- src/domain/converters/product-types.ts
+++ src/domain/converters/product-types.ts
@@ -1,24 +1,22 @@
 import { products_3 } from '../graphql-types';
 import { Product } from '../models';
 
-
 function descriptionLegible(p: products_3) {
-    if (p.licensesMax === 1) {
-        return 'Single user';
-    } else {
-        return 'Team of ' + p.licensesMin + '-' + p.licensesMax + ' users';
-    }
+  if (p.licensesMax === 1) {
+    return `1 user`;
+  } else {
+    return `${p.licensesMin}-${p.licensesMax} users`;
+  }
 }
 
-
 export function productFromProduct_3(p: products_3): Product {
-    return {
-        id: p.id,
-        name: p.name,
-        description: descriptionLegible(p),
-        licensesMin: p.licensesMin,
-        licensesMax: p.licensesMax,
-        pricereg: p.pricereg,
-        pricesale: p.pricesale
-    };
+  return {
+    id: p.id,
+    name: p.name,
+    description: descriptionLegible(p),
+    licensesMin: p.licensesMin,
+    licensesMax: p.licensesMax,
+    pricereg: p.pricereg,
+    pricesale: p.pricesale
+  };
 }
