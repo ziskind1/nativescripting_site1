22137b93b2de7974b6f3643bdbcccc79f280c50f
Alexander Ziskind <alex@nuvious.com>
2019-01-02 23:05:49 -0500

    added isUrlExternal check

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,3 +1,24 @@
 export function getAuthorImgSrc(authorImageName: string): string {
-    return `/img/authors/${authorImageName}`;
+  return `/img/authors/${authorImageName}`;
+}
+
+export function isUrlExternal(url: string) {
+  const host = window.location.hostname;
+
+  const linkHost = (function(url) {
+    if (/^https?:\/\//.test(url)) {
+      // Absolute URL.
+      // The easy way to parse an URL, is to create <a> element.
+      // @see: https://gist.github.com/jlong/2428561
+      const parser = document.createElement('a');
+      parser.href = url;
+
+      return parser.hostname;
+    } else {
+      // Relative URL.
+      return window.location.hostname;
+    }
+  })(url);
+
+  return host !== linkHost;
 }
