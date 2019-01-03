bfdba957da1581074b810e2cab7e57250dc70c75
Alexander Ziskind <alex@nuvious.com>
2019-01-02 22:50:24 -0500

    breadcrumbs Link instead of anchor tag

--- src/components/shared/BreadCrumbs/BreadCrumbs.tsx
+++ src/components/shared/BreadCrumbs/BreadCrumbs.tsx
@@ -1,40 +1,40 @@
-import * as React from "react";
+import * as React from 'react';
+import { Link } from 'gatsby';
 
 import './BreadCrumbs.css';
 
 export interface BreadCrumb {
-    name: string;
-    url: string;
+  name: string;
+  url: string;
 }
 
 export interface BreadCrumbsProps {
-    breadcrumbs: BreadCrumb[];
+  breadcrumbs: BreadCrumb[];
 }
 
+export const BreadCrumbs: React.StatelessComponent<BreadCrumbsProps> = (
+  props: BreadCrumbsProps
+) => {
+  const listHtml = props.breadcrumbs.map((bc, idx) => {
+    if (idx === props.breadcrumbs.length - 1) {
+      return (
+        <li key={idx}>
+          <span>{bc.name}</span>
+        </li>
+      );
+    } else {
+      return (
+        <li key={idx}>
+          <Link to={bc.url}>{bc.name}</Link>
+          <img src="/img/arrow.png" />
+        </li>
+      );
+    }
+  });
 
-export const BreadCrumbs: React.StatelessComponent<BreadCrumbsProps> = (props: BreadCrumbsProps) => {
-    const listHtml = props.breadcrumbs.map((bc, idx) => {
-        if (idx === props.breadcrumbs.length - 1) {
-            return (
-                <li key={idx}>
-                    <span>{bc.name}</span>
-                </li>
-            );
-        } else {
-            return (
-                <li key={idx}>
-                    <a href={bc.url}>{bc.name}</a>
-                    <img src="/img/arrow.png" />
-                </li>
-            );
-        }
-    });
-
-    return (
-        <div className="breadcrumbs-container">
-            <ul>
-                {listHtml}
-            </ul>
-        </div>
-    );
-}
+  return (
+    <div className="breadcrumbs-container">
+      <ul>{listHtml}</ul>
+    </div>
+  );
+};
