f603c7ee1b679c404592c968c6c7320b5bbd465c
Alexander Ziskind <alex@Alexs-MBP16.fios-router.home>
2019-12-29 22:46:08 -0500

    added guarantee section

--- src/components/home/BundleSection/BundleSection.tsx
+++ src/components/home/BundleSection/BundleSection.tsx
@@ -7,7 +7,61 @@ import BundleCardList from '../BundleCard/BundleCardList';
 
 import './BundleSection.css';
 import { PaymentTypes } from '../../shared/PaymentTypes/PaymentTypes';
+import { GridRow } from '../../../layouts/grid2/grid-row';
+import { GridColumn } from '../../../layouts/grid2/grid-column';
 
+const ProdGuaranteeWrapper = styled.div`
+    position: relative;
+    padding-left: 80px;
+    padding-right: 30px;
+    padding-bottom: 40px;
+`;
+
+const GuaranteeTitleRefund = styled.h3`
+  font-size: 2.2rem;
+  margin-bottom: 5px;
+
+  :before {
+    content: "";
+    width: 64px;
+    height: 64px;
+    position: absolute;
+    top: -22px;
+    left: 0;
+
+    background: url(/img/benefits/guarantee.svg) center center no-repeat;
+    background-size: cover;
+
+    @media only screen and (max-width: 767px) {
+        width: 48px;
+        height: 48px;
+        top: -16px;
+    }
+  }
+`;
+
+const GuaranteeTitleDiscount = styled.h3`
+  font-size: 2.2rem;
+  margin-bottom: 5px;
+
+  :before {
+    content: "";
+    width: 64px;
+    height: 64px;
+    position: absolute;
+    top: -22px;
+    left: 0;
+
+    background: url(/img/benefits/discount.svg) center center no-repeat;
+    background-size: cover;
+
+    @media only screen and (max-width: 767px) {
+      width: 48px;
+      height: 48px;
+      top: -16px;
+    }
+  }
+`;
 
 export interface BundleSectionProps {
   bundles: Bundle[];
@@ -38,6 +92,21 @@ const BundleSection: React.StatelessComponent<BundleSectionProps> = (
         <BundleCardList bundles={props.bundles} />
 
         <PaymentTypes/>
+
+        <GridRow>
+          <GridColumn xs={12} sm={6}>
+            <ProdGuaranteeWrapper>
+              <GuaranteeTitleRefund> 100% money back guarantee </GuaranteeTitleRefund>
+              <p> These courses are empowering thousands with expert knowledge - they will you too. If you're disappointed for whatever reason, you'll get every cent back. </p>
+            </ProdGuaranteeWrapper>
+          </GridColumn>
+          <GridColumn xs={12} sm={6}>
+            <ProdGuaranteeWrapper>
+              <GuaranteeTitleDiscount> Student/Location discounts </GuaranteeTitleDiscount>
+              <p> Not-so-good exchange rate? (i.e. India/Brazil) Get in touch for a coupon. Student? Email us from your student email or attach your student ID. </p>
+            </ProdGuaranteeWrapper>
+          </GridColumn>
+        </GridRow>
         
       </div>
     </Section>
