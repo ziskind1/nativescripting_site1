624c71b819bb3a1c05dc39df434de64c59801bcb
Alexander Ziskind <alex@nuvious.com>
2020-03-18 14:17:12 -0400

    added google tag manager

--- src/html.tsx
+++ src/html.tsx
@@ -42,7 +42,21 @@ export default class extends React.Component<HtmlProps, void> {
       <html lang="en">
         <head>
           {this.props.headComponents}
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-M4KR6VX');</script>
+                            `
+            }}
+          />
+
           {head.title.toComponent()}
+          <script>alert('test');</script>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
@@ -94,22 +108,6 @@ export default class extends React.Component<HtmlProps, void> {
             dangerouslySetInnerHTML={{ __html: this.props.body }}
           />
           {this.props.postBodyComponents}
-
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                                (function (i, s, o, g, r, a, m) {
-                                    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-                                        (i[r].q = i[r].q || []).push(arguments)
-                                    }, i[r].l = 1 * new Date(); a = s.createElement(o),
-                                        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-                                })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-                        
-                                ga('create', 'UA-98718768-1', 'auto');
-                                ga('send', 'pageview');
-                            `
-            }}
-          />
         </body>
       </html>
     );
