ce9f6d4d02258540d0806ad33933ae1781206e74
Alexander Ziskind <alex@nuvious.com>
2020-03-21 14:29:19 -0400

    added ad video to webinar page

--- src/pages/pro-webinar.tsx
+++ src/pages/pro-webinar.tsx
@@ -6,7 +6,10 @@ import { MainLayout } from '../layouts/MainLayout';
 import { Seo } from '../components/shared/Seo/Seo';
 import { ContentPageWrapper } from '../layouts/page-layouts/content-page';
 import { Wrapper } from '../layouts/page-layouts/wrapper';
-import { PageHeadingMain, PageHeading2 } from '../components/shared/PageHeaders';
+import {
+  PageHeadingMain,
+  PageHeading2
+} from '../components/shared/PageHeaders';
 import { PageSection } from '../layouts/page-layouts/page-section';
 
 import { GridRow } from '../layouts/grid2/grid-row';
@@ -15,77 +18,107 @@ import { ContactComponent } from '../components/contact/contact.component';
 import WebinarCountdownTimer from '../components/shared/WebinarCountdownTimer/WebinarCountdownTimer';
 
 const CTAWrapper = styled.div`
-    margin: auto;
-    margin-top: 50px;
-    text-align: center;
+  margin: auto;
+  margin-top: 50px;
+  text-align: center;
 `;
 
 const CTA = styled.button`
-    background-color: #e43145;
-    border: none;
-    color: white;
-    font-size: 26px;
-    line-height: 130%;
-    font-weight: 500;
-    padding: 25px;
-    width: 70%;
-    white-space: normal;
-    cursor: pointer;
-    text-transform: uppercase;
-    box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+  background-color: #e43145;
+  border: none;
+  color: white;
+  font-size: 26px;
+  line-height: 130%;
+  font-weight: 500;
+  padding: 25px;
+  width: 70%;
+  white-space: normal;
+  cursor: pointer;
+  text-transform: uppercase;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 
-    :hover {
-        background-color: #bd2234;
-    }
+  :hover {
+    background-color: #bd2234;
+  }
 `;
 
-interface ProWebinarPageProps { }
-interface ProWebinarPageState { }
+const VidWrapper = styled.div`
+  height: 300px;
+`;
 
-export default class extends React.Component<ProWebinarPageProps, ProWebinarPageState> {
+interface ProWebinarPageProps {}
+interface ProWebinarPageState {}
 
-    public componentDidMount() {
-        this.mountWebinarScript();
-    }
+export default class extends React.Component<
+  ProWebinarPageProps,
+  ProWebinarPageState
+> {
+  public componentDidMount() {
+    this.mountWebinarScript();
+  }
 
-    private mountWebinarScript() {
-        const script = document.createElement("script");
-        script.src = `//events.genndi.com/register.evergreen.extra.js`;
-        script.async = true;
-        document.body.appendChild(script);
-    }
+  private mountWebinarScript() {
+    const script = document.createElement('script');
+    script.src = `//events.genndi.com/register.evergreen.extra.js`;
+    script.async = true;
+    document.body.appendChild(script);
+  }
 
-    public render() {
-        const pageTitle = `NativeScript Pro Webinar | NativeScript Courses`;
+  public render() {
+    const pageTitle = `NativeScript Pro Webinar | NativeScript Courses`;
 
-        return (
-            <MainLayout>
-                <Seo path="/pro-webinar" />
-                <Helmet>
-                    <title>{pageTitle}</title>
-                </Helmet>
+    return (
+      <MainLayout>
+        <Seo path="/pro-webinar" />
+        <Helmet>
+          <title>{pageTitle}</title>
+        </Helmet>
 
-                <ContentPageWrapper>
-                    <PageSection>
-                        <Wrapper>
+        <ContentPageWrapper>
+          <PageSection>
+            <Wrapper>
+              <div className="video-container">
+                <div className="about-top-block">
+                  <PageHeadingMain center>
+                    Online training webinar begins in{' '}
+                    <WebinarCountdownTimer style={{ color: '#e43145' }} />
+                  </PageHeadingMain>
 
+                  <VidWrapper className="video-wrapper sm">
+                    <iframe
+                      width="640"
+                      height="360"
+                      src="https://player.vimeo.com/video/399471866"
+                      frameBorder="0"
+                      allowFullScreen
+                    />
+                  </VidWrapper>
+                </div>
+              </div>
 
-                            <PageHeadingMain center>Online training webinar begins in <WebinarCountdownTimer style={{ color: '#e43145' }} /></PageHeadingMain>
+              <CTAWrapper>
+                <CTA
+                  type="button"
+                  className=""
+                  title="regpopbox_819005285141055972_623cfc39cd"
+                >
+                  <span>Yes, I want to join the free training webinar</span>
+                </CTA>
+              </CTAWrapper>
 
-
-                            <CTAWrapper>
-                                <CTA type="button" className="" title="regpopbox_819005285141055972_623cfc39cd">
-                                    <span>Yes, I want to join the free training webinar</span>
-                                </CTA>
-                            </CTAWrapper>
-
-                            <img src="//events.genndi.com/tracker?action=registration-evergreen&webicode=623cfc39cd&version=&memberid=819005285141055972" style={{ visibility: 'hidden', height: '0px', width: '0px', border: 'none' }}></img>
-
-                        </Wrapper>
-                    </PageSection>
-                </ContentPageWrapper>
-
-            </MainLayout>
-        );
-    }
+              <img
+                src="//events.genndi.com/tracker?action=registration-evergreen&webicode=623cfc39cd&version=&memberid=819005285141055972"
+                style={{
+                  visibility: 'hidden',
+                  height: '0px',
+                  width: '0px',
+                  border: 'none'
+                }}
+              ></img>
+            </Wrapper>
+          </PageSection>
+        </ContentPageWrapper>
+      </MainLayout>
+    );
+  }
 }
