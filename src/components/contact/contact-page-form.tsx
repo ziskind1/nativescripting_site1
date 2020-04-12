f10d246981d6cbf46c12b71d52d7378fc8f5a735
Alexander Ziskind <alex@nuvious.com>
2020-04-12 14:13:51 -0400

    footer links update

--- src/components/contact/contact-page-form.tsx
+++ src/components/contact/contact-page-form.tsx
@@ -4,202 +4,193 @@ import styled from 'styled-components';
 import { handleFormSubmit } from '../../utils/form-utils';
 import { colors } from '../../global/colors';
 
-
-interface ContactPageFormProps { }
+interface ContactPageFormProps {}
 
 interface ContactPageFormState {
-    submitted: boolean;
-    [key: string]: any;
+  submitted: boolean;
+  [key: string]: any;
 }
 
-
-
 const SingleInputWrapper = styled.div`
-margin-bottom: 20px;
+  margin-bottom: 20px;
 `;
 
-
-
 const ILabel = styled.label`
-    color: ${colors.titleBlue};
-    padding-top: 0;
+  color: ${colors.titleBlue};
+  padding-top: 0;
 `;
 
 const ISpan = styled.span`
-    position: relative;
-    display: block;
-    width: 100%;
-    margin-top: 5px;
+  position: relative;
+  display: block;
+  width: 100%;
+  margin-top: 5px;
 `;
 
 const IInput = styled.input`
-    margin: 0;
-    vertical-align: baseline;
-    width: 100%;
+  margin: 0;
+  vertical-align: baseline;
+  width: 100%;
 
-    appearance: none;
+  appearance: none;
 
-    font-weight: 400;
-    font-size: 1.6rem;
-    color: ${colors.titleGreen};
-    padding: 5px 10px;
+  font-weight: 400;
+  font-size: 1.6rem;
+  color: ${colors.titleGreen};
+  padding: 5px 10px;
 
-    outline: none;
-    line-height: 28px;
+  outline: none;
+  line-height: 28px;
 
-
-    background-color: #032e74;
-    border: 1px solid #12ffcd;
-    box-shadow: 0px 0px 0px 0px #E6E9EC;
+  background-color: #032e74;
+  border: 1px solid #12ffcd;
+  box-shadow: 0px 0px 0px 0px #e6e9ec;
 `;
 
 const ITextarea = styled.textarea`
-    margin: 0;
-    vertical-align: baseline;
-    width: 100%;
+  margin: 0;
+  vertical-align: baseline;
+  width: 100%;
 
-    appearance: none;
+  appearance: none;
 
-    box-shadow: inset 0 2px 0 rgba(84,94,111,.2);
+  box-shadow: inset 0 2px 0 rgba(84, 94, 111, 0.2);
 
-    font-weight: 400;
-    font-size: 1.6rem;
-    color: ${colors.titleGreen};
-    padding: 5px 10px;
+  font-weight: 400;
+  font-size: 1.6rem;
+  color: ${colors.titleGreen};
+  padding: 5px 10px;
 
+  outline: none;
+  line-height: 28px;
 
-    outline: none;
-    line-height: 28px;
-
-    background-color: #032e74;
-    border: 1px solid #12ffcd;
-    box-shadow: 0px 0px 0px 0px #E6E9EC;
+  background-color: #032e74;
+  border: 1px solid #12ffcd;
+  box-shadow: 0px 0px 0px 0px #e6e9ec;
 `;
 
 const IInvisible = styled.input`
-    visibility: hidden;
+  visibility: hidden;
 `;
 
 const ISubmit = styled.input`
-    position: relative;
-    height: 56px;
-    cursor: pointer;
-    text-align: center;
-    overflow: hidden;
-    background-color: ${colors.actionPurple};
+  position: relative;
+  height: 56px;
+  cursor: pointer;
+  text-align: center;
+  overflow: hidden;
+  background-color: ${colors.actionPurple};
 
-    font-family: MonoRegular;
-    font-size: 16px;
-    font-weight: 700;
-    color: ${colors.white};
-    text-transform: uppercase;
-    line-height: 56px;
-    text-decoration: none;
-    border: none;
-    width: 50%;
+  font-family: MonoRegular;
+  font-size: 16px;
+  font-weight: 700;
+  color: ${colors.white};
+  text-transform: uppercase;
+  line-height: 56px;
+  text-decoration: none;
+  border: none;
+  width: 50%;
 `;
 
 export class ContactPageForm extends React.Component<
-    ContactPageFormProps,
-    ContactPageFormState
-    > {
-    constructor(props: ContactPageFormProps) {
-        super(props);
+  ContactPageFormProps,
+  ContactPageFormState
+> {
+  constructor(props: ContactPageFormProps) {
+    super(props);
 
-        this.state = {
-            submitted: false,
-            name: '',
-            email: '',
-            message: '',
-        };
-    }
+    //const q = (this.props as any)
+    console.log('QUERY: ', this.props);
 
-    public handleChange(e: any) {
-        this.setState({ [e.target.name]: e.target.value });
-    }
+    this.state = {
+      submitted: false,
+      name: '',
+      email: '',
+      message: ''
+    };
+  }
 
-    public handleSubmit(event: React.FormEvent<EventTarget>) {
-        handleFormSubmit(event, this.state).then(() => {
-            this.setState({ submitted: true });
-        });
-    }
+  public handleChange(e: any) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-    public render() {
+  public handleSubmit(event: React.FormEvent<EventTarget>) {
+    handleFormSubmit(event, this.state).then(() => {
+      this.setState({ submitted: true });
+    });
+  }
 
+  public render() {
+    return (
+      <div>
+        {this.state.submitted && (
+          <div>Thank you. We will get back to you shortly.</div>
+        )}
 
+        {!this.state.submitted && (
+          <form
+            name="contact-page-form"
+            method="post"
+            onSubmit={e => this.handleSubmit(e)}
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+          >
+            <input type="hidden" name="form-name" value="contact-page-form" />
 
-        return (
-            <div>
-                {this.state.submitted && (
-                    <div>Thank you. We will get back to you shortly.</div>
-                )}
+            <SingleInputWrapper>
+              <ILabel>
+                Name *
+                <ISpan>
+                  <IInput
+                    type="text"
+                    name="name"
+                    required
+                    onChange={e => this.handleChange(e)}
+                    value={this.state['name']}
+                  />
+                </ISpan>
+              </ILabel>
+            </SingleInputWrapper>
 
-                {!this.state.submitted && (
-                    <form
-                        name="contact-page-form"
-                        method="post"
-                        onSubmit={e => this.handleSubmit(e)}
-                        data-netlify="true"
-                        data-netlify-honeypot="bot-field"
-                    >
-                        <input type="hidden" name="form-name" value="contact-page-form" />
+            <SingleInputWrapper>
+              <ILabel>
+                Email *
+                <ISpan>
+                  <IInput
+                    type="email"
+                    name="email"
+                    required
+                    onChange={e => this.handleChange(e)}
+                    value={this.state['email']}
+                  />
+                </ISpan>
+              </ILabel>
+            </SingleInputWrapper>
 
-                        <SingleInputWrapper>
-                            <ILabel>Name *
-                                <ISpan>
-                                    <IInput type="text"
-                                        name="name"
-                                        required
-                                        onChange={e => this.handleChange(e)}
-                                        value={this.state['name']}
-                                    />
-                                </ISpan>
-                            </ILabel>
-                        </SingleInputWrapper>
+            <SingleInputWrapper>
+              <ILabel>
+                Message *
+                <ISpan>
+                  <ITextarea
+                    name="message"
+                    required
+                    onChange={e => this.handleChange(e)}
+                    value={this.state['message']}
+                  />
+                </ISpan>
+              </ILabel>
+            </SingleInputWrapper>
 
-                        <SingleInputWrapper>
-                            <ILabel>Email *
-                            <ISpan>
-                                    <IInput
-                                        type="email"
-                                        name="email"
-                                        required
-                                        onChange={e => this.handleChange(e)}
-                                        value={this.state['email']}
-                                    />
-                                </ISpan>
-                            </ILabel>
-                        </SingleInputWrapper>
+            <div data-netlify-recaptcha />
 
-                        <SingleInputWrapper>
-                            <ILabel>Message *
-                            <ISpan>
-                                    <ITextarea
-                                        name="message"
-                                        required
-                                        onChange={e => this.handleChange(e)}
-                                        value={this.state['message']}
-                                    />
-                                </ISpan>
-                            </ILabel>
-                        </SingleInputWrapper>
+            <SingleInputWrapper style={{ textAlign: 'center' }}>
+              <ISubmit type="submit" value="Send" />
+            </SingleInputWrapper>
 
-                        <div data-netlify-recaptcha />
-
-                        <SingleInputWrapper style={{ textAlign: 'center' }}>
-                            <ISubmit
-                                type="submit"
-                                value="Send"
-                            />
-                        </SingleInputWrapper>
-
-                        <IInvisible
-                            name="bot-field"
-                            onChange={e => this.handleChange(e)}
-                        />
-                    </form>
-                )}
-            </div>
-        );
-    }
+            <IInvisible name="bot-field" onChange={e => this.handleChange(e)} />
+          </form>
+        )}
+      </div>
+    );
+  }
 }
