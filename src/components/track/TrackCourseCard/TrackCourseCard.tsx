d6832ef6b1301992267d6867201e5846ef3b3e28
Alexander Ziskind <alex@nuvious.com>
2020-10-13 10:10:53 -0400

    pulled style comps out of render function

--- src/components/track/TrackCourseCard/TrackCourseCard.tsx
+++ src/components/track/TrackCourseCard/TrackCourseCard.tsx
@@ -9,6 +9,7 @@ import LevelIcon from '../../LevelIcon/LevelIcon';
 // import './CourseCard2.css';
 import { getAuthorImgSrc } from '../../../utils/urls';
 
+
 interface TrackCourseCardProps {
     course: Course;
     onSelect?: () => void;
@@ -136,7 +137,53 @@ font-weight: 700;
 margin-left: 6px;
 `;
 
-export const TrackCourseCard: React.StatelessComponent<TrackCourseCardProps> = (
+interface CourseTopProps {
+    courseImage: string;
+}
+
+const CourseTop = styled.div` 
+    width: 100%;
+    height: 180px;
+    position: relative;
+    z-index: 0;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-image: url(${(props: CourseTopProps)=>props.courseImage}), linear-gradient( 45deg, #0949b2 66%, #0845a9 66%, #0845a9 78%, #0a429c 78% );
+
+    @media (max-width: 640px)
+    {
+            height: 200px;
+            background-size: 300px;
+            background-position: 50%;
+            background-color:  #0949b2;
+            background-image: url(${props=>(props as any).courseImage});
+    }
+    `;
+
+interface BadgeProps {
+    label: string;
+}
+
+const Badge = styled.div`
+    background-color: #b517ec;
+    padding: 3px 6px 0;
+    width: 100px;
+    margin-top: 10px;
+    display: ${(props: BadgeProps) => props.label ? 'block' : 'none'};
+
+    :before {
+        content: " ";
+        display: table;
+    }
+
+    :after {
+        clear: both;
+        content: " ";
+        display: table;
+    }
+    `;
+
+export const TrackCourseCard: React.FunctionComponent<TrackCourseCardProps> = (
     props: TrackCourseCardProps
 ) => {
     const course = props.course;
@@ -179,61 +226,15 @@ export const TrackCourseCard: React.StatelessComponent<TrackCourseCardProps> = (
         );
 
     const levelHtml = getCourseLevelHtml(course);
-
-
     const courseImage = `/img/illustrations/transparent_bg/${course.url}.svg`;
-
-    const CourseTop = styled.div`
-        
-        width: 100%;
-        height: 180px;
-        position: relative;
-        z-index: 0;
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-image: url(${courseImage}), linear-gradient( 45deg, #0949b2 66%, #0845a9 66%, #0845a9 78%, #0a429c 78% );
-    
-        @media (max-width: 640px)
-        {
-                height: 200px;
-                background-size: 300px;
-                background-position: 50%;
-                background-color:  #0949b2;
-                background-image: url(${courseImage});
-        }
-        `;
-
-
-
-
-    const Badge = styled.div`
-    background-color: #b517ec;
-    padding: 3px 6px 0;
-    width: 100px;
-    margin-top: 10px;
-    display: ${course.label ? 'block' : 'none'};
-
-    :before {
-        content: " ";
-        display: table;
-    }
-
-    :after {
-        clear: both;
-        content: " ";
-        display: table;
-    }
-    `;
-
-
 
     return (
 
         <TrackCourseBlock>
             <Link to={'/course/' + course.url}>
                 <CourseBox>
-                    <CourseTop>
-                        <Badge>
+                    <CourseTop courseImage={courseImage}>
+                        <Badge label={course.label}>
                             <BadgeIcon><svg id="icon-interactive" viewBox="0 0 24 24" width="16px" height="16px"><g><path d="M22 10.054h-2.025V5.997H3.996V17.98h8.051V20H3a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v5.054zm-3.346 5.178l3.199 3.199a.503.503 0 0 1 0 .71l-.711.712a.503.503 0 0 1-.71 0l-3.2-3.2-.937 1.875a.503.503 0 0 1-.911-.027l-3.343-7.8a.503.503 0 0 1 .66-.66l7.8 3.343a.503.503 0 0 1 .027.911l-1.874.937z"></path></g></svg>
                             </BadgeIcon>
                             <BadgeText>{course.label}</BadgeText>
