e3775150176e13ae48df7760a62492c38b43f74a
Alex Ziskind <alex@nuvious.com>
2023-01-13 13:14:18 -0500

    added test rename script to shrink images

---
+++ rename.sh
@@ -0,0 +1,9 @@
+#! /bin/zsh -
+filelist=$(find data/posts -name '*.jpg')
+for image_file in $filelist
+do
+    echo $image_file
+    convert $image_file -resize 50% $image_file
+#latest=$(/TiffOut/*.tiff(.om[1]))
+#magick $image_file $latest -evaluate-sequence Max '/TiffOut/out_${imagefile}.tiff'
+done 
